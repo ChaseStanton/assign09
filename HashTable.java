6c5f0160e2da931ce6b81e0ba6d69eff1fca7abf
Reece Kalmar <ralmarkalmar@gmail.com>
2023-11-14 18:42:37 -0700

    f

--- HashTable.java
+++ HashTable.java
@@ -6,12 +6,11 @@ import java.util.List;
 public class HashTable<K, V> implements Map<K, V> {
 	private ArrayList<MapEntry<K, V>> table;
 	private int items;
-	
-	
-	public HashTable(){
+
+	public HashTable() {
 		this.table = new ArrayList<MapEntry<K, V>>(7);
 	}
-	
+
 	@Override
 	public void clear() {
 		this.items = 0;
@@ -24,13 +23,13 @@ public class HashTable<K, V> implements Map<K, V> {
 		int index = hash(key);
 		int startingIndex = index;
 		int i = 1;
-		
-		while(table.get(index) != null) {
-			if(table.get(index).getKey().equals(key))
+
+		while (table.get(index) != null) {
+			if (table.get(index).getKey().equals(key))
 				return true;
 			index = (startingIndex + i * i) % capacity;
 			i++;
-			if(startingIndex == index)
+			if (startingIndex == index)
 				break;
 		}
 		return false;
@@ -38,8 +37,8 @@ public class HashTable<K, V> implements Map<K, V> {
 
 	@Override
 	public boolean containsValue(V value) {
-		for(MapEntry<K, V> entry : this.table) {
-			if(entry.getValue().equals(value))
+		for (MapEntry<K, V> entry : this.table) {
+			if (entry.getValue().equals(value))
 				return true;
 		}
 		return false;
@@ -48,8 +47,8 @@ public class HashTable<K, V> implements Map<K, V> {
 	@Override
 	public List<MapEntry<K, V>> entries() {
 		List<MapEntry<K, V>> list = new ArrayList<>();
-		for(MapEntry<K, V> entry : this.table) {
-			if(entry != null)
+		for (MapEntry<K, V> entry : this.table) {
+			if (entry != null)
 				list.add(entry);
 		}
 		return list;
@@ -61,13 +60,13 @@ public class HashTable<K, V> implements Map<K, V> {
 		int index = hash(key);
 		int startingIndex = index;
 		int i = 1;
-		
-		while(table.get(index) != null) {
-			if(table.get(index).getKey().equals(key))
+
+		while (table.get(index) != null) {
+			if (table.get(index).getKey().equals(key))
 				return table.get(index).getValue();
 			index = (startingIndex + i * i) % capacity;
 			i++;
-			if(startingIndex == index)
+			if (startingIndex == index)
 				break;
 		}
 		return null;
@@ -83,13 +82,13 @@ public class HashTable<K, V> implements Map<K, V> {
 		this.items += 1;
 		V replacedValue;
 		int hashIndex = hash(key);
-		if(this.table.get(hashIndex) != null){
+		if (this.table.get(hashIndex) != null) {
 			replacedValue = this.table.get(hashIndex).getValue();
-			this.table.set(hashIndex, new MapEntry<K,V>(key, value));
+			this.table.set(hashIndex, new MapEntry<K, V>(key, value));
 			return replacedValue;
 		}
-		this.table.set(hashIndex, new MapEntry<K,V>(key, value));
-			return null;
+		this.table.set(hashIndex, new MapEntry<K, V>(key, value));
+		return null;
 	}
 
 	@Override
@@ -98,8 +97,8 @@ public class HashTable<K, V> implements Map<K, V> {
 		int index = hash(key);
 		int startingIndex = index;
 		int i = 1;
-		while(table.get(index) == null) {
-			if(table.get(index).getKey().equals(key)) {
+		while (table.get(index) == null) {
+			if (table.get(index).getKey().equals(key)) {
 				V value = table.get(index).getValue();
 				table.set(index, null);
 				this.items--;
@@ -107,7 +106,7 @@ public class HashTable<K, V> implements Map<K, V> {
 			}
 			index = (startingIndex + i * i) % capacity;
 			i++;
-			if(index == startingIndex)
+			if (index == startingIndex)
 				break;
 		}
 		return null;
@@ -117,13 +116,12 @@ public class HashTable<K, V> implements Map<K, V> {
 	public int size() {
 		return this.items;
 	}
-	
+
 	private int hash(K key) {
 		int capacity = this.table.size();
 		int hashCode = key.hashCode();
 		int hashIndex = Math.abs(hashCode) % capacity;
 		return hashIndex;
 	}
-	
 
 }
